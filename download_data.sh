c27698c70fe2270343acb5d910845945f8709312
yangyi02 <yangyi02@gmail.com>
2018-08-06 11:47:52 -0700

    finalize download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,8 @@
-# wget https://www.dropbox.com/s/5zarkyny7ywc2fv/PROXEMICS.zip?dl=0
-# mv PROXEMICS.zip?dl=0 PROXEMICS.zip
+wget https://www.dropbox.com/s/5zarkyny7ywc2fv/PROXEMICS.zip?dl=0
+mv PROXEMICS.zip?dl=0 data/PROXEMICS.zip
+unzip data/PROXEMICS.zip -d data
 
-# wget https://www.dropbox.com/s/jtnticywxulfnq6/INRIA.zip?dl=0 
-# mv INRIA.zip?dl=0 INRIA.zip
+wget https://www.dropbox.com/s/jtnticywxulfnq6/INRIA.zip?dl=0 
+mv INRIA.zip?dl=0 data/INRIA.zip
+unzip data/INRIA.zip -d data
+
